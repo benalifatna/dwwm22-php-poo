1de79ce0b65bf69e5e3b4ca7ed93d7613c09a2e7
Fatna <benalifatna02@gmail.com>
2025-12-31 13:55:24 +0100

    interface et trait

---
+++ 6-interface.php
@@ -0,0 +1,93 @@
+<?php
+
+
+    // interface Actions
+    interface Actions {
+        public function respirer(): void;
+        public function seNourrir(): void;
+        public function dormir(): void;
+        public function seReproduire(): void;
+    }
+
+
+    // Classe abstraite Animal
+    abstract class Animal implements Actions {
+
+        protected ?string $nom = null;
+        protected ?string $age = null;
+
+        public function respirer(): void {
+
+        }
+
+        public function seNourrir(): void {
+
+        }
+
+        public function dormir(): void {
+
+        }
+
+        public function seReproduire(): void {
+
+        }
+    }
+
+
+    // Classes concrêtes Chien, Chat
+    final class Chien extends Animal {
+        
+    }
+
+    class Hello extends Chien {
+        
+    }
+
+
+// --------------------------------------------------------------------
+
+
+    // interface Actions {
+    //     public function appeler(): void;
+    //     public function envoyerMessage(): void;
+    //     public function photographier(): void;
+    // }
+
+    // class Telephone implements Actions {
+    //     public function appeler(): void {
+
+    //     }
+
+    //     public function envoyerMessage(): void {
+
+    //     }
+
+    //     public function photographier(): void {
+
+    //     }
+    // }
+
+// --------------------------------------------------------------------
+
+    // interface Actions {
+    //     public function manger();
+    //     public function dormir();
+    //     public function crier();
+    // }
+
+    // class Chien implements Actions {
+    //     public function manger() {
+
+    //     }
+
+    //     public function dormir() {
+
+    //     }
+    //     public function crier() {
+
+    //     }
+
+    //     public function courir() {
+
+    //     }
+    // }
